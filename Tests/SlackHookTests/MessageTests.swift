1828f61b450aa4aaecfcb1f88edbe8f6767c9349
Graeme Read <graeme@optimisedlabs.com>
2018-01-02 20:44:56 +0000

    Be consistent with whitespace, naming and ensure complete message test is useful and DRY

--- Tests/SlackHookTests/MessageTests.swift
+++ Tests/SlackHookTests/MessageTests.swift
@@ -88,7 +88,6 @@ class MessageTests: XCTestCase {
     func testAttachmentWithMarkDown() {
         let text = "_This_ is *really* ~good~ bad example `code`: ```print('Hello World')```"
         let attachment = Attachment(colour: nil, text: text, title: nil, markdownUsedIn: [.text], fields: nil)
-        
         let message = Message(text: nil, username: nil, attachments: [attachment])
         let expectedJson = """
         {
@@ -107,12 +106,11 @@ class MessageTests: XCTestCase {
     }
     
     func testAttachmentWithFields() {
-        let value = "This field could be really large and span multiple lines"
         let shortField1 = Field(title: "Short 1", value: "Value 1")
         let shortField2 = Field(title: "Short 2", value: "Value 2")
-        let longField = Field(title: "Long", short: false, value: value)
+        let longValue = "This field could be really large and span multiple lines"
+        let longField = Field(title: "Long", short: false, value: longValue)
         let attachment = Attachment(colour: nil, text: nil, title: nil, markdownUsedIn: nil, fields: [shortField1, shortField2, longField])
-        
         let message = Message(text: nil, username: nil, attachments: [attachment])
         let expectedJson = """
         {
@@ -132,7 +130,7 @@ class MessageTests: XCTestCase {
                 {
                   "short"\(padding): false,
                   "title"\(padding): "Long",
-                  "value"\(padding): "\(value)"
+                  "value"\(padding): "\(longValue)"
                 }
               ]
             }
@@ -144,42 +142,46 @@ class MessageTests: XCTestCase {
     }
     
     func testCompleteMessage() {
-        let attachment1 = Attachment(colour: nil, text: nil, title: "Title1", markdownUsedIn: nil, fields: nil)
-        let attachment2 = Attachment(colour: .good, text: "This is some text", title: "Title2", markdownUsedIn: [.fields], fields: nil)
-        let attachment3 = Attachment(colour: .warning, text: nil, title: "Title3", markdownUsedIn: [.fields], fields: nil)
-        let attachment4 = Attachment(colour: .danger, text: nil, title: "Title4", markdownUsedIn: nil, fields: nil)
+        let text = "This is the message text"
+        let attachmentText = "This is some text inside the attachment"
         
-        let message = Message(
-            text: nil,
-            username: "Custom Username",
-            attachments: [attachment1, attachment2, attachment3, attachment4]
-        )
+        let shortField1 = Field(title: "Short 1", value: "Value 1")
+        let shortField2 = Field(title: "Short 2", value: "Value 2")
+        let longValue = "This field could be really large and span multiple lines"
+        let longField = Field(title: "Long", short: false, value: longValue)
+        
+        let attachment = Attachment(colour: .warning, text: attachmentText, title: "Title2", markdownUsedIn: [.fields], fields: [longField, shortField2, shortField1])
+        let message = Message(text: text, username: "Custom Username", attachments: [attachment])
         let expectedJson = """
         {
           "attachments"\(padding): [
             {
-              "title"\(padding): "Title1"
-            },
-            {
-              "color"\(padding): "good",
-              "mrkdwn_in"\(padding): [
-                "fields"
-              ],
-              "text"\(padding): "This is some text",
-              "title"\(padding): "Title2"
-            },
-            {
               "color"\(padding): "warning",
+              "fields"\(padding): [
+                {
+                  "short"\(padding): false,
+                  "title"\(padding): "Long",
+                  "value"\(padding): "\(longValue)"
+                },
+                {
+                  "short"\(padding): true,
+                  "title"\(padding): "Short 2",
+                  "value"\(padding): "Value 2"
+                },
+                {
+                  "short"\(padding): true,
+                  "title"\(padding): "Short 1",
+                  "value"\(padding): "Value 1"
+                }
+              ],
               "mrkdwn_in"\(padding): [
                 "fields"
               ],
-              "title"\(padding): "Title3"
-            },
-            {
-              "color"\(padding): "danger",
-              "title"\(padding): "Title4"
+              "text"\(padding): "\(attachmentText)",
+              "title"\(padding): "Title2"
             }
           ],
+          "text"\(padding): "\(text)",
           "username"\(padding): "Custom Username"
         }
         """
